54fb8a747a7976d374f4874aad1a5eb7ba464ee4
vivski <vnls1@aluno.ifal.edu.br>
2024-07-03 21:40:53 -0300

    volte 5 casas p entender pq que as funçoes nao estao funcionando corretamente; e o css; boa noite.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,35 @@ document.addEventListener("readystatechange", (event) => {
 const initApp = () =>{
     refreshThePage();
 }
-const refresh =()=>{
+const refreshThePage =()=>{
   clearListDisplay();
+  renderList();
+  clearItemEntry();
+  setFocusOnItemField();
+
+
+}
+
+const clearListDisplay = () =>{
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+}
+
+const deleteContents = (parentElement) =>{
+    let child = parentElement.lastElementChild;
+    while(child){
+        parentElement.removeChild(child)
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderList = () =>{
+    const list = toDoList.getList();
+    list.forEach((item) => {
+        buildListItem(item);
+    });
+};
+
+const buildListItem = (item) => {
 
 }
