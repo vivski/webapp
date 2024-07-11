ccb28488d81ab762ea70d989eaaf00852b1ced77
vivski <vnls1@aluno.ifal.edu.br>
2024-07-10 22:05:55 -0300

    !#

--- js/todolist.js
+++ js/todolist.js
@@ -6,11 +6,12 @@ export default class ToDoList {
         return this._list;
     }
     clearList(){
-        this._list=[];
+        this._list= [];
     }
     addItemToList(itemObj){
         this._list.push(itemObj);
     }
+    
     removeItemFromList(id){
         const list = this._list;
         for(let i = 0; i < list.length; i++){
