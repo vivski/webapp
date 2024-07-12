51b37647eaf02e3d49df28a5df353a04a85da52a
vivski <vnls1@aluno.ifal.edu.br>
2024-07-11 21:25:24 -0300

    !js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import ToDoList from "./todolist.js";
+import ToDoItem from "./todoitem.js";
+
+const toDoList = new ToDoList();
+
+
---
+++ js/todoitem.js
@@ -0,0 +1,19 @@
+export default class ToDoItem{
+    constructor(){
+        this._id = null;
+        this._item = null;
+    }
+    getId(){
+        return this._id;
+    }
+    setId(id){
+        this._id = id;
+    }
+    getItem(){
+        this._item = item;
+    }
+    setItem(item){
+        this._item = item;
+    }
+}
+
